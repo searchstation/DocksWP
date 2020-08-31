d5e932ac0422eed12125c57235e1e30ce3cd6491
searchstation <alex@cobadigital.com>
2020-08-31 11:19:15 +0100

    cleanup sidebar

--- sidebar.php
+++ sidebar.php
@@ -5,12 +5,6 @@
  ?>
 
  <div class="large-4 medium-5 small-12 cell cell">
-   <ul class="menu vertical sidebar" id="menu">
-     <?php docks_menu(3); ?>
-   </ul>
-   <div data-sticky-container>
-   <div class="sticky" data-sticky data-top-anchor="menu:bottom" data-btm-anchor="content:bottom">
-   <?php get_template_part('/parts/content-sidebar'); ?>
-   </div>
-   </div>
+
+
  </div>
